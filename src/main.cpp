1557c9b112221983f8f320b0fa5bb08d22e8289c
Sergey Kosarevsky <sk@linderdaum.com>
2021-07-05 18:34:13 +0300

    Initial Block declarations

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,53 @@
-﻿#include <stdio.h>
+﻿#include <stdint.h>
+#include <stdio.h>
 
 #include "sha2.h"
 
+#if _MSC_VER
+#	pragma warning(disable:4200) // nonstandard extension used: zero-sized array in struct/union
+#endif // _MSC_VER
+
+struct Block
+{
+	uint32_t metaData_ = 0;
+	uint64_t nonce_ = 0;
+	uint64_t timeStamp_ = 0; // milliseconds
+	char hashPrev_[SHA512_DIGEST_STRING_LENGTH] = { 0 };
+	char hash_[SHA512_DIGEST_STRING_LENGTH] = { 0 };
+	uint16_t payloadSize_ = 0;
+	uint8_t payload_[];
+};
+
+void calculateHash(Block& block)
+{
+	SHA512_CTX ctx;
+
+	SHA512_Init(&ctx);
+	SHA512_Update(&ctx, (const uint8_t*)&block, sizeof(Block) + block.payloadSize_);
+	SHA512_End(&ctx, block.hash_);
+}
+
+bool checkDifficulty(const Block& block, uint8_t difficulty)
+{
+	assert(difficulty < SHA512_DIGEST_STRING_LENGTH);
+
+	SHA512_CTX ctx;
+
+	char hash[SHA512_DIGEST_STRING_LENGTH] = { 0 };
+
+	SHA512_Init(&ctx);
+	SHA512_Update(&ctx, (const uint8_t*)&block, sizeof(Block) + block.payloadSize_);
+	SHA512_End(&ctx, hash);
+
+	for (uint8_t i = 0; i != difficulty; i++)
+		if (hash[i] != '0') return false;
+
+	return true;
+}
+
 int main()
 {
+	Block block;
+
 	return 0;
 }
